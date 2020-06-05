cf4b12252b55a7e564411b7ec4098e293ca6acc7
amanjagdev <amankumarjagdev@gmail.com>
2020-06-05 16:39:29 +0530

    test Analysis complete

--- api/analysis.js
+++ api/analysis.js
@@ -1,12 +1,50 @@
 const express = require("express");
 const router = express.Router();
+const BrainJs = require("brain.js");
+const natural = require("natural");
+const TrainingSet = require('../data/trainingData');
+
+function buildWordDictionary(trainingData) {
+    const tokenisedArray = trainingData.map(item => {
+        const tokens = item.phrase.split(' ')
+        return tokens.map(token => natural.PorterStemmer.stem(token))
+    })
+
+    const flattenedArray = [].concat.apply([], tokenisedArray)
+    return flattenedArray.filter((item, pos, self) => self.indexOf(item) == pos)
+}
+
+const dictionary = buildWordDictionary(TrainingSet)
+
+function encode(phrase) {
+    const phraseTokens = phrase.split(' ')
+    const encodedPhrase = dictionary.map(word => phraseTokens.includes(word) ? 1 : 0)
+
+    return encodedPhrase
+}
+
+const encodedTrainingSet = TrainingSet.map(dataSet => {
+    const encodedValue = encode(dataSet.phrase)
+    return { input: encodedValue, output: dataSet.result }
+})
+
+
+const network = new BrainJs.NeuralNetwork()
+network.train(encodedTrainingSet)
+
+const encoded = encode("Im so happy to have cae")
+
+
 
 /*
-    @route:     /api/analyse/
-    @desc:      To analyse whether the given sentence is possitive or not
+@route:     /api/analyse/
+@desc:      To analyse whether the given sentence is possitive or not
 */
-router.get('/',(req,res) => {
-    res.send("Analysed");
+router.get('/', (req, res) => {
+    
+    let result = network.run(encoded);
+    result = JSON.stringify(result);
+    res.send("Analysed" + result);
 });
 
 
